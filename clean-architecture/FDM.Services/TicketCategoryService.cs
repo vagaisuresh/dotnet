7e7d6938960573e5e7ffe7f09242543877447898
Suresh <38977541+vagaisuresh@users.noreply.github.com>
2024-07-02 12:23:37 +0530

    Add files via upload

---
+++ clean-architecture/FDM.Services/TicketCategoryService.cs
@@ -0,0 +1,85 @@
+﻿using FDM.Domain.Models;
+using FDM.Domain.Repositories;
+using FDM.Domain.Services;
+using FDM.Domain.Services.Communication;
+
+namespace FDM.Services
+{
+    public class TicketCategoryService : ITicketCategoryService
+    {
+        private readonly ITicketCategoryRespository _respository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public TicketCategoryService(ITicketCategoryRespository respository, IUnitOfWork unitOfWork)
+        {
+            _respository = respository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IEnumerable<Ticketcategory>> ListAsync()
+        {
+            return await _respository.ListAsync();
+        }
+
+        public async Task<TicketCategoryResponse> SaveAsync(Ticketcategory ticketcategory)
+        {
+            try
+            {
+                await _respository.AddAsync(ticketcategory);
+                await _unitOfWork.CompleteAsync();
+
+                return new TicketCategoryResponse(ticketcategory);
+            }
+            catch (Exception ex)
+            {
+                // Do some logging stuff
+                return new TicketCategoryResponse($"An error occurred when saving the ticket category: {ex.Message}");
+            }
+        }
+
+        public async Task<TicketCategoryResponse> UpdateAsync(short id, Ticketcategory ticketcategory)
+        {
+            var existingCategory = await _respository.FindByIdAsync(id);
+
+            if (existingCategory == null)
+                return new TicketCategoryResponse("Ticket category not found.");
+
+            existingCategory.CategoryName = ticketcategory.CategoryName;
+            existingCategory.IsActive = ticketcategory.IsActive;
+
+            try
+            {
+                _respository.Update(existingCategory);
+                await _unitOfWork.CompleteAsync();
+
+                return new TicketCategoryResponse(existingCategory);
+            }
+            catch (Exception ex)
+            {
+                // Do some logging stuff
+                return new TicketCategoryResponse($"An error occurred when updating the category: {ex.Message}");
+            }
+        }
+
+        public async Task<TicketCategoryResponse> DeleteAsync(short id)
+        {
+            var existingCategory = await _respository.FindByIdAsync(id);
+
+            if (existingCategory == null)
+                return new TicketCategoryResponse("Ticket category not found.");
+
+            try
+            {
+                _respository.Remove(existingCategory);
+                await _unitOfWork.CompleteAsync();
+
+                return new TicketCategoryResponse(existingCategory);
+            }
+            catch (Exception ex)
+            {
+                // Do some logging stuff
+                return new TicketCategoryResponse($"An error occurred when deleting the category: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ clean-architecture/FDM.Services/TicketService.cs
@@ -0,0 +1,85 @@
+﻿using FDM.Domain.Models;
+using FDM.Domain.Repositories;
+using FDM.Domain.Services;
+using FDM.Domain.Services.Communication;
+
+namespace FDM.Services
+{
+    public class TicketService : ITicketService
+    {
+        private readonly ITicketRespository _respository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public TicketService(ITicketRespository respository, IUnitOfWork unitOfWork)
+        {
+            _respository = respository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<IEnumerable<Ticket>> ListAsync()
+        {
+            return await _respository.ListAsync();
+        }
+
+        public async Task<TicketResponse> SaveAsync(Ticket ticket)
+        {
+            try
+            {
+                await _respository.AddAsync(ticket);
+                await _unitOfWork.CompleteAsync();
+
+                return new TicketResponse(ticket);
+            }
+            catch (Exception ex)
+            {
+                // Do some logging stuff
+                return new TicketResponse($"An error occurred when saving the ticket: {ex.Message}");
+            }
+        }
+
+        public async Task<TicketResponse> UpdateAsync(int id, Ticket ticket)
+        {
+            var existingTicket = await _respository.FindByIdAsync(id);
+
+            if (existingTicket == null)
+                return new TicketResponse("Ticket not found.");
+
+            existingTicket.TicketDate = ticket.TicketDate;
+            /// OTHER FIELDS
+
+            try
+            {
+                _respository.Update(existingTicket);
+                await _unitOfWork.CompleteAsync();
+
+                return new TicketResponse(existingTicket);
+            }
+            catch (Exception ex)
+            {
+                // Do some logging stuff
+                return new TicketResponse($"An error occurred when updating the ticket: {ex.Message}");
+            }
+        }
+
+        public async Task<TicketResponse> DeleteAsync(int id)
+        {
+            var existingTicket = await _respository.FindByIdAsync(id);
+
+            if (existingTicket == null)
+                return new TicketResponse("Ticket not found.");
+
+            try
+            {
+                _respository.Remove(existingTicket);
+                await _unitOfWork.CompleteAsync();
+
+                return new TicketResponse(existingTicket);
+            }
+            catch (Exception ex)
+            {
+                // Do some logging stuff
+                return new TicketResponse($"An error occurred when deleting the ticket: {ex.Message}");
+            }
+        }
+    }
+}
