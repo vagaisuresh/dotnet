5e073924d0d9c802dbbd73d5d3a14b769451cb4c
Suresh Baskar <38977541+vagaisuresh@users.noreply.github.com>
2024-08-20 22:23:50 +0530

    Update Book.cs

--- unit-testing/LibraryApp/LibraryApi/Book.cs
+++ unit-testing/LibraryApp/LibraryApi/Book.cs
@@ -7,6 +7,7 @@ namespace LibraryApi
         public Guid Id { get; set; }
         [Required]
         public string? Title { get; set; }
+        [Required]
         public string? Author { get; set; }
         public string? Description { get; set; }
     }
